b6c382354835c770e7b467c4c923c4a365bdac74
Hamza Eşsiz <webcatdev@gmail.com>
2022-07-06 10:44:54 +0300

    fixed error in downloaded pdfs and added deleting functionality to the server

--- server/index.js
+++ server/index.js
@@ -1,39 +1,41 @@
-const path = require('path')
+const path = require("path");
+const fsPromises = require("fs").promises;
 const express = require("express");
 const app = express();
 const pdf = require("html-pdf");
 const cors = require("cors");
+const { v4: uuid } = require("uuid");
 
-var whitelist = ["http://127.0.0.1:3000", "http://localhost:3000"];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 
 app.post("/create-pdf", async (req, res) => {
-  console.log(req.body);
+  const id = uuid();
   pdf
     .create(`<h1 style="color: red; font-size: 4rem">${req.body.text}</h1>`)
-    .toFile("./newPDF.pdf", async (err) => {
+    .toFile(path.join(__dirname, "pdf", `${id}.pdf`), (err) => {
       if (err) res.send(Promise.reject());
-      res.send(Promise.resolve())
-    })
-
+      res.send(id);
+    });
 });
 
-app.get('/fetch-pdf', (req, res) => {
-    res.setHeader("Content-Type" , "application/octet-stream; charset=utf-8")
-    res.setHeader("Content-Disposition", 'attachment; filename="newPDF.pdf"')
-    res.sendFile(path.join(__dirname, "newPDF.pdf"));
-})
+app.get("/fetch-pdf/:id", async (req, res) => {
+  const id = req.params.id;
+  const filePath = path.join(__dirname, "pdf", `${id}.pdf`);
+  res.setHeader("Content-Type", "application/octet-stream; charset=utf-8");
+  res.setHeader("Content-Disposition", 'attachment; filename="newPDF.pdf"');
+  res.sendFile(filePath, async () => {
+    try {
+      await fsPromises.unlink(filePath);
+    } catch (err) {
+      console.log(err);
+    }
+     
+   });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Üzgünüz. Bu sayfa henüz oluşturulmadı.");
+});
 
 app.listen(5000);
